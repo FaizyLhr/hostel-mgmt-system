32fc457f18532d35e9275d10218cba49a58d50bc
Faizy <FaizyLhr>
2021-12-15 12:20:08 +0500

    Added Constant Beds

---
+++ server/constants/beds.js
@@ -0,0 +1,23 @@
+const beds = [
+	{ isFree: true, allocatedTo: null, bedNum: 1 },
+	{ isFree: true, allocatedTo: null, bedNum: 2 },
+	{ isFree: true, allocatedTo: null, bedNum: 3 },
+	{ isFree: true, allocatedTo: null, bedNum: 4 },
+	{ isFree: true, allocatedTo: null, bedNum: 5 },
+	{ isFree: true, allocatedTo: null, bedNum: 6 },
+	{ isFree: true, allocatedTo: null, bedNum: 7 },
+	{ isFree: true, allocatedTo: null, bedNum: 8 },
+	{ isFree: true, allocatedTo: null, bedNum: 9 },
+	{ isFree: true, allocatedTo: null, bedNum: 10 },
+	{ isFree: true, allocatedTo: null, bedNum: 11 },
+	{ isFree: true, allocatedTo: null, bedNum: 12 },
+	{ isFree: true, allocatedTo: null, bedNum: 13 },
+	{ isFree: true, allocatedTo: null, bedNum: 14 },
+	{ isFree: true, allocatedTo: null, bedNum: 15 },
+	{ isFree: true, allocatedTo: null, bedNum: 16 },
+	{ isFree: true, allocatedTo: null, bedNum: 17 },
+	{ isFree: true, allocatedTo: null, bedNum: 18 },
+	{ isFree: true, allocatedTo: null, bedNum: 19 },
+	{ isFree: true, allocatedTo: null, bedNum: 20 },
+];
+module.exports = beds;
