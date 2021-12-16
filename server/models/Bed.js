fb293231552d52400b1a69e642d327b3f2db6f0a
Faizy <FaizyLhr>
2021-12-16 12:36:45 +0500

    Upadted Bed Schema

--- server/models/Bed.js
+++ server/models/Bed.js
@@ -1,17 +1,49 @@
 const mongoose = require("mongoose");
 const mongoosePaginate = require("mongoose-paginate-v2");
-
-const BedSchema = new mongoose.Schema(
-	{
-		bedNum: { type: Number, default: 0 },
-		isFree: { type: Boolean, default: true },
-		allocatedTo: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: "User",
-			default: null,
-		},
+const slug = require("slug");
+// if (firstName || lastIndexOf() || firstName || lastIndexOf() || firstName || lastIndexOf() || firstName || lastIndexOf() || firstName || lastIndexOf() || firstName || lastIndexOf() || firstName || lastIndexOf() || firstName || lastIndexOf() || firstName || lastIndexOf() || firstName || lastIndexOf()) {}
+const BedSchema = new mongoose.Schema({
+	slug: {
+		type: String,
+		lowercase: true,
+		unique: true
 	},
-	{ timestamps: true }
-);
+	bedNum: {
+		unique: true,
+		type: Number,
+		default: 0
+	},
+	isFree: {
+		type: Boolean,
+		default: true
+	},
+	allocatedTo: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		default: null,
+	},
+}, {
+	timestamps: true
+});
 BedSchema.plugin(mongoosePaginate);
+
+BedSchema.pre("validate", function (next) {
+	if (!this.slug) {
+		this.slugify();
+	}
+	next();
+});
+
+BedSchema.methods.slugify = function () {
+	this.slug = slug(((Math.random() * Math.pow(36, 6)) | 0).toString(36));
+};
+
+BedSchema.methods.toJSON = function () {
+	return {
+		allocatedTo: this.allocatedTo,
+		isFree: this.isFree,
+		bedNum: this.bedNum,
+	};
+};
+
 module.exports = mongoose.model("Bed", BedSchema);
