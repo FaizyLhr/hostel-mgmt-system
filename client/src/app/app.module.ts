3112c83151f19b80702d8adee2ab40bfdfb518bf
Faizy <FaizyLhr>
2021-12-16 17:35:47 +0500

    Added Components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './MyComponents/signin/signin.component';
+import { HomeComponent } from './MyComponents/home/home.component';
+import { SignUpComponent } from './MyComponents/sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    HomeComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
