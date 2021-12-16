845788a7a87731708ae2a8d5e1c37e6189b40b3e
Faizy <FaizyLhr>
2021-12-16 20:04:59 +0500

    Routing Done

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SigninComponent } from './MyComponents/signin/signin.component';
+import { HomeComponent } from './MyComponents/home/home.component';
+import { SignUpComponent } from './MyComponents/sign-up/sign-up.component';
+import { ProfileComponent } from './MyComponents/profile/profile.component';
+import { BoardAdminComponent } from './MyComponents/board-admin/board-admin.component';
+import { BoardModeratorComponent } from './MyComponents/board-moderator/board-moderator.component';
+import { BoardUserComponent } from './MyComponents/board-user/board-user.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: SigninComponent },
+  { path: 'register', component: SignUpComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: 'mod', component: BoardModeratorComponent },
+  { path: 'admin', component: BoardAdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
