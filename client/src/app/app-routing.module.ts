d37c2d21dfcdbd7dd1badb49d5da65c801d526b1
Faizy <FaizyLhr>
2021-12-16 23:57:01 +0500

    Reset Client

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,23 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { SigninComponent } from './MyComponents/signin/signin.component';
-import { HomeComponent } from './MyComponents/home/home.component';
-import { SignUpComponent } from './MyComponents/sign-up/sign-up.component';
-import { ProfileComponent } from './MyComponents/profile/profile.component';
-import { BoardAdminComponent } from './MyComponents/board-admin/board-admin.component';
-import { BoardModeratorComponent } from './MyComponents/board-moderator/board-moderator.component';
-import { BoardUserComponent } from './MyComponents/board-user/board-user.component';
+
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'login', component: SigninComponent },
-  { path: 'register', component: SignUpComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'user', component: BoardUserComponent },
-  { path: 'mod', component: BoardModeratorComponent },
-  { path: 'admin', component: BoardAdminComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,30 +6,14 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SigninComponent } from './MyComponents/signin/signin.component';
-import { HomeComponent } from './MyComponents/home/home.component';
-import { SignUpComponent } from './MyComponents/sign-up/sign-up.component';
-import { NavbarComponent } from './MyComponents/navbar/navbar.component';
-import { ProfileComponent } from './MyComponents/profile/profile.component';
-import { BoardAdminComponent } from './MyComponents/board-admin/board-admin.component';
-import { BoardModeratorComponent } from './MyComponents/board-moderator/board-moderator.component';
-import { BoardUserComponent } from './MyComponents/board-user/board-user.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { authInterceptorProviders } from './Helpers/auth.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SigninComponent,
-    HomeComponent,
-    SignUpComponent,
-    NavbarComponent,
-    ProfileComponent,
-    BoardAdminComponent,
-    BoardModeratorComponent,
-    BoardUserComponent,
+    
   ],
   imports: [
     BrowserModule,
