10067e2bd753ed1e8e7065475b7574533615f6c3
Faizy <FaizyLhr>
2021-12-16 20:05:29 +0500

    App CMP

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,12 +9,13 @@ import { AppComponent } from './app.component';
 import { SigninComponent } from './MyComponents/signin/signin.component';
 import { HomeComponent } from './MyComponents/home/home.component';
 import { SignUpComponent } from './MyComponents/sign-up/sign-up.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './MyComponents/navbar/navbar.component';
 import { ProfileComponent } from './MyComponents/profile/profile.component';
 import { BoardAdminComponent } from './MyComponents/board-admin/board-admin.component';
 import { BoardModeratorComponent } from './MyComponents/board-moderator/board-moderator.component';
 import { BoardUserComponent } from './MyComponents/board-user/board-user.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { authInterceptorProviders } from './Helpers/auth.interceptor';
 
